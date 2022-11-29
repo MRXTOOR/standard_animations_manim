71be9db7da02a89d1ac0e9acf72092d8b88a9823
Harry <57110073+MRXTOOR@users.noreply.github.com>
2022-11-29 13:37:35 +0300

    Add files via upload

--- tz1.py
+++ tz1.py
@@ -3,53 +3,35 @@ from numpy import *
 from pathlib import Path
 
 
-class VectorArrow(MovingCameraScene):
+class FilledAngle(Scene):
   def construct(self):
-    quote = Tex("Mathematics is the only perfect...")
-    quote.set_color(YELLOW_A)
-    quote.to_edge(UP)
-    quote2 = Tex("Doing calculations, you get into a mess ... realize it")
-    quote2.set_color(YELLOW_B)
-    author = Tex("-Albert Einstein")
-    author.scale(0.75)
-    author.next_to(quote.get_corner(DOWN + RIGHT), DOWN)
+    # --------------------------------------------------------------------------------------------------------
+    numberplane = NumberPlane()
 
-    self.add(quote)
-    self.add(author)
-    self.wait(2)
-    self.play(Transform(quote, quote2), ApplyMethod(author.move_to, quote2.get_corner(DOWN + RIGHT) + DOWN + 2 * LEFT))
-    self.play(ApplyMethod(author.match_color, quote2), Transform(author, author.scale(1)))
-    self.play(FadeOut(quote))
-    self.play(FadeOut(author))
-    math = MathTex("5^2+x^{4+5}gx5", "=", "\sin\left(a^2d\right)=d\sqrt{d5}")
+    l1 = Line(ORIGIN, 2 * RIGHT, ).set_color(GREEN_E).rotate(0 * DEGREES, about_point=ORIGIN)
+    l2 = Line(ORIGIN, 4 * RIGHT, ).set_color(GREEN_D).rotate(30 * DEGREES, about_point=ORIGIN)
+    l3 = Line(ORIGIN, 3.90 * RIGHT, ).set_color(GREEN_C).rotate(50 * DEGREES, about_point=ORIGIN)
+    #  arc = Angle(ORIGIN,2 * RIGHT)
+    # l2 = Line(ORIGIN, 2 * UP + LEFT).set_color(GREEN).rotate(-10 * DEGREES, about_point=ORIGIN)
+    # l3 = Arrow(ORIGIN, 3 *  LEFT).set_color(GREEN).rotate(-35 * DEGREES, about_point=ORIGIN).
+    # self.play(Create(l1))
+    # arcBetweenPoints=ArcBetweenPoints(start=np.array([0,0,1]),end=np.array([1,0,0]))
+    # -----------------------------------------------------------------------------------------------------------
+    # self.play(Create(arcBetweenPoints))
+    self.play(Create(numberplane), run_time=4.0)
+    self.play(Create(l1), run_time=4.0)
 
-    uwu1 = SurroundingRectangle(math[0], buff=.1)
-    uwu2 = SurroundingRectangle(math[2], buff=.1)
+    self.play(Create(l2), run_time=4.0)
 
-    code = ''' # Write math
-        math = MathTex("5^2+x^{4+5}gx5", "=", "\sin\left(a^2d\right)=d\sqrt{d5}")
+    self.play(Create(l3), run_time=6.0)
 
-        self.play(Write(math))
+    self.play(Create((Arc(angle=PI / 6))), run_time=1.0)
 
-        uwu1 = SurroundingRectangle(math[0], buff=.1)
-        uwu2 = SurroundingRectangle(math[2], buff=.1)
+    self.play(Create((Arc(angle=PI / 3.60).set_color(YELLOW_A))), run_time=4.0)
 
-        self.play(Create(uwu1)) 
-        self.wait()
-        self.play(ReplacementTransform(uwu1, uwu2))
-        self.wait()
-'''
+    self.play(FadeOut(numberplane), run_time=0.2)
+    self.play(FadeOut(l1), run_time=0.2)
+    self.play(FadeOut(l2), run_time=0.2)
+    self.play(FadeOut(l3), run_time=0.2)
 
-    rendered_code = Code(code=code, tab_width=4, background="window",
-                         language="Python", font="Monospace")
-    self.add(rendered_code)
-    self.play(FadeOut(rendered_code), run_time=5.2)
-    self.play(Write(math))
 
-    self.play(Create(uwu1))
-
-    self.wait()
-
-    self.play(ReplacementTransform(uwu1, uwu2))
-
-    self.wait()
